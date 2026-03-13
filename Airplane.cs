5644d18452df4ce7900255754e8a236c42e1ece3
test name <test email>
2026-03-13 18:58:32 +0200

    Initial commit

---
+++ Airplane.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project4
+{
+    //Entity - Airplane
+    class Airplane
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Model { get; set; }
+        //Relationship type : one to many (1...*)
+        public int MaxCountPassangers { get; set; }
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project4
+{
+    [Table("Passangers")]
+    class Client
+    {
+        //Primary Key naming : Id/id/ID/ EntityName+Id (ClientId)
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("FirstName")]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime? Birthdate { get; set; }//value type null
+
+        public int Rating { get; set; }
+
+        //null ---> not null
+        //not null ---> null
+
+        //Relationship type : many to many (*...*)
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
